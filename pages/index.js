36b54ba5587a39ed83238bd6e7c899e58c0c3a4f
Okah Prosper <prosperoluwadamilare@gmail.com>
2025-10-15 15:22:20 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import AnimatedIntro from "../components/AnimatedIntro";
+import { useRouter } from "next/router";
+
+export default function Home() {
+  const [entered, setEntered] = useState(false);
+  const router = useRouter();
+
+  const handleEnter = () => {
+    setEntered(true);
+    setTimeout(() => router.push("/dashboard"), 700);
+  };
+
+  return (
+    <>
+      {!entered ? <AnimatedIntro onEnter={handleEnter} /> : (
+        <div className="min-h-screen flex items-center justify-center">
+          <p>Loading StackFi...</p>
+        </div>
+      )}
+    </>
+  );
+}
